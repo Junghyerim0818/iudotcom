f51873c7572ef87b4e8da1af0698c915b53873e0
Junghyerim <thsehddnr68@gmail.com>
2025-11-24 15:43:42 +0900

    Add files via upload

--- run.py
+++ run.py
@@ -1,14 +1,9 @@
 """
-로컬 개발용 실행 스크립트
+프로덕션/배포용 실행 스크립트
 """
 import os
-
-# 로컬 개발 환경에서 HTTPS 요구사항 무시
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-
-# index.py의 app을 import하여 사용
 from index import app
 
 if __name__ == '__main__':
-    # 로컬 개발용 설정
-    app.run(debug=True, host='0.0.0.0', port=80)
+    # 프로덕션 환경 설정
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
